11a38c291981f7817aa479884ee94746d69e4df3
Taras-Veremchuk <113293007+Taras-Veremchuk@users.noreply.github.com>
2023-06-18 21:50:49 +0000

    Deploying to gh-pages from @ Taras-Veremchuk/goit-react-hw-08-phonebook@64d9644cf281158d64054b96edfd7bbb7dafc53d 🚀

--- static/js/266.14907a16.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[266],{1266:function(e,t,n){n.r(t),n.d(t,{default:function(){return c}});var r=n(184),i={container:{minHeight:"calc(100vh - 50px)",display:"flex",alignItems:"center",justifyContent:"center"},title:{fontWeight:500,fontSize:48,textAlign:"center"}};function c(){return(0,r.jsx)("div",{style:i.container,children:(0,r.jsx)("h1",{style:i.title,children:"It's your personal phonebook"})})}}}]);
-//# sourceMappingURL=266.14907a16.chunk.js.map
